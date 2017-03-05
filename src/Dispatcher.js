bfa0df80eaec645cf3048b3278e095db97aa2b9c
Joseph Dalrymple <joseph.dalrymple@bluelogicteam.com>
2017-03-04 20:44:24 -0600

    Updated `when()` multiple parameter parachute error message

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -16,8 +16,8 @@ export default class Dispatcher {
 		});
 	}
 
-	when(eventName, invalidFunc) {
-		if(invalidFunc !== void 0) {
+	when(eventName) {
+		if(arguments.length > 1) {
 			throw new TypeError('when: Only one parameter is allowed. Did you mean to write .when().then()?');
 		}
 		this.log(`when(${eventName})`, this.LOG_LOUD);
